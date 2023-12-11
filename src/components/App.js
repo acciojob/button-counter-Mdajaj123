8afece6bb4bfed67ed325b675533c8026516a12b
unknown <mdijaz2596@gmail.com>
2023-12-11 17:36:00 +0530

    new change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [a, fun] = useState(0);
+  function increase() {
+    fun(a + 1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {a} times</p>
+      <button onClick={increase}>Click me</button>
     </div>
   )
 }
